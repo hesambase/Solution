d1bf8513f9327aaef54c2cdcb109b25da20db5ae
hesam attari <Hesam_att@outlook.com>
2022-11-23 11:10:13 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SolutionTest;
+
+Solution _solution = new Solution();
+int lenght = 0;
+int power;
+Console.WriteLine("please enter the size tokens bag");
+lenght = System.Convert.ToInt32(Console.ReadLine());
+int[] tokens = new int[lenght];
+
+Console.WriteLine("please enter the tokens value(power)");
+for (int i = 0; i < lenght; i++)
+    tokens[i] = System.Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("please enter the input power");
+power = System.Convert.ToInt32(Console.ReadLine());
+int output=_solution.BagOfTokensScore(tokens, power);
+Console.WriteLine(output);
---
+++ Solution.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolutionTest
+{
+    public class Solution
+    {
+        public int BagOfTokensScore(int[] tokens, int power)
+        {
+            Array.Sort(tokens);
+            int i = 0;
+            int j = tokens.Length - 1;
+            int points = 0;
+            int res = 0;
+            while (i <= j)
+            {
+                if (power >= tokens[i])
+                {
+                    power -= tokens[i++];
+                    res = Math.Max(res, ++points);
+                }
+                else if (points > 0)
+                {
+                    points--;
+                    power += tokens[j--];
+                }
+                else
+                {
+                    break;
+                }
+
+
+
+            }
+            return res;
+        }
+
+    }
+    }
+
